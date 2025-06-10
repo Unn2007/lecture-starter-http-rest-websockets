5fd192063b235b4c6f029a9c9e716bc672b0f774
unn2007 <unn2007@i.ua>
2025-06-10 20:36:02 +0300

    + Added socket connection handling and Implemented /game/texts/:id route

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -2,12 +2,26 @@ import { Router } from "express";
 import path from "node:path";
 
 import { HTML_FILES_PATH } from "../config.js";
+import { texts } from "../data.js";
 
 const router = Router();
+
+const MIN_TEXT_ID = 0;
+const NOT_FOUND_STATUS = 404;
 
 router.get("/", (_request, response) => {
     const page = path.join(HTML_FILES_PATH, "game.html");
     response.sendFile(page);
+});
+
+router.get("/texts/:id", (request, response) => {
+    const textId = Number.parseInt(request.params.id, 10);
+
+    if (textId >= MIN_TEXT_ID && textId < texts.length) {
+        response.send(texts[textId]);
+    } else {
+        response.status(NOT_FOUND_STATUS).send("Text not found");
+    }
 });
 
 export { router };
--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,8 +1,23 @@
 import { type Server } from "socket.io";
 
+const users = new Map();
+
 const socketHandler = (io: Server): void => {
     io.on("connection", (socket) => {
-        const { username: _username } = socket.handshake.query;
+        const { username } = socket.handshake.query;
+
+        if (users.has(username)) {
+            socket.emit("username_error", { message: "This username is already in use. Please choose another one." });
+            socket.disconnect();
+
+            return;
+        }
+
+        users.set(username, socket.id);
+
+        socket.on("disconnect", () => {
+            users.delete(username);
+        });
     });
 };
 
